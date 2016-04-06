e393aca16da16d58f4380364b2c649b84d8184ed
KENNY <kennylai360@gmail.com>
2016-04-06 15:05:17 +0100

    Added Boundary and that whenever an object leaves the boundary it will be destroyed.

---
+++ Space Shooter/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+* Only used by the Boundary game object for when a game object leaves the screen
+for example a projectile, it will destroy it. prevent any clogging the system's memory
+*/
+
+public class DestroyByBoundary : MonoBehaviour {
+
+	void OnTriggerExit(Collider other) {
+		Destroy(other.gameObject);
+	}
+}

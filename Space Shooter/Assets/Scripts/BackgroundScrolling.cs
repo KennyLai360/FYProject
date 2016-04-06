30380e2a968552254928a9e032c1331594cea420
KENNY <kennylai360@gmail.com>
2016-04-06 20:13:01 +0100

    Added scrolling background. NOTE: Probably should comment out the BackgroundScrolling.cs file

---
+++ Space Shooter/Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScrolling : MonoBehaviour {
+	
+	public float scrollSpeed;
+	public float tileSizeZ;
+	
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveBG = new Vector3(1.0f, 0.0f, 0.0f);
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + moveBG * newPosition;
+	}
+}

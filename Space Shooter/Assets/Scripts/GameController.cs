0b16c746843767efcdd6c442be2cc28f75ea208c
KENNY <kennylai360@gmail.com>
2016-04-06 16:11:26 +0100

    Added spawn of waves and position of spawn of waves.

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnPositionValues;
+	
+	public int hazardCount;
+	public float beginningEnemySpawnWait;
+	public float spawnWait;
+	public float timeForNextWave;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnWaves());
+	}
+	
+	IEnumerator SpawnWaves() {
+		//Wait for X amount of seconds before spawning the enemy wave so the player can prepare.
+		yield return new WaitForSeconds(beginningEnemySpawnWait);
+		
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3(spawnPositionValues.x, 0.0f, Random.Range(-spawnPositionValues.z, spawnPositionValues.z));
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(timeForNextWave);
+		}
+	}
+	
+}
